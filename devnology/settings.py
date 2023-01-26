9f05afcf97934fc6388ed28f97cf4e19b15b5faa
By-Lucas <lukasmulekezika2@gmail.com>
2023-01-25 23:19:06 -0300

    active connection aws

--- devnology/settings.py
+++ devnology/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS', cast=Csv())]
 
 APPS_DIR = sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
